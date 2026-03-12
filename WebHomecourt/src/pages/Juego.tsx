15fa6bec92cf849dfeb98590483d940794058302
Cielo Maria Vega Godoy <a01741542@tec.mx>
2026-03-12 10:07:55 -0600

    Add Unity WebGL build files and package-lock.json
    - Created RETO.loader.js for Unity WebGL instance management.
    - Added RETO.wasm file for WebAssembly support.
    - Initialized package-lock.json for dependency management.

--- WebHomecourt/src/pages/Juego.tsx
+++ WebHomecourt/src/pages/Juego.tsx
@@ -1,11 +1,34 @@
 import Nav from '../components/Nav'
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Juego() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/Build/RETO.loader.js",
+    dataUrl: "/Build/RETO.data",
+    frameworkUrl: "/Build/RETO.framework.js",
+    codeUrl: "/Build/RETO.wasm",
+  });
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold">Juego</h1>
+    <div className="flex flex-col items-center">
+
+      <h1 className="text-5xl font-bold mt-6">Juego</h1>
       <Nav current="Juego" />
+
+      <div className="mt-8 w-full flex justify-center">
+        <Unity
+          unityProvider={unityProvider}
+          className="
+            w-full
+            h-[60vh]
+            md:w-[960px]
+            md:h-[540px]
+          "
+        />
+      </div>
+
     </div>
+
   )
 }
 
