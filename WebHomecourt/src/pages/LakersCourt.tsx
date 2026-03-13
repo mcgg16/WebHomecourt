f7773b2d792649bb3563e809c3fcd002c707018d
Cielo Maria Vega Godoy <a01741542@tec.mx>
2026-03-12 18:41:11 -0600

    Refactor layout in LakersCourt component for improved structure and styling; adjust card display in DisplayUserCards for consistent height and image constraints

--- WebHomecourt/src/pages/LakersCourt.tsx
+++ WebHomecourt/src/pages/LakersCourt.tsx
@@ -5,22 +5,26 @@ import ShowEvents from '../components/ShowEvents'
 function LakersCourt() {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center p-10">
-        <h1 className="text-5xl font-bold">LakersCourt</h1>
+      <div className="flex flex-col items-center justify-center">
         <Nav current="LakersCourt" />
       </div>
-      
-      <div>
-        <h1 className="text-2xl font-bold p-5">Eventos</h1>
-        <div className="flex items-center justify-center">
-        <NewEvent/>
-        <ShowEvents/>
+      <div className='px-14 py-5 bg-zinc-100 w-full '>
+        <div className="w-full px-5 py-7 bg-violet-950 rounded-2xl shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] outline outline-1 outline-offset-[-1px] outline-black/25 inline-flex flex-col justify-start items-start gap-3.5 overflow-hidden">
+          <h1 className="justify-start text-zinc-100">LakersCourt</h1>
         </div>
-      </div>
-      <div>
-        <h1 className="text-2xl font-bold p-5">Mapa de canchas</h1>
-        <div className="flex items-center justify-center">
-        <Map />
+        
+        <div>
+          <h1 className="text-2xl font-bold p-5">Eventos</h1>
+          <div className="flex items-center justify-center">
+          <NewEvent/>
+          <ShowEvents/>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold p-5">Mapa de canchas</h1>
+          <div className="flex items-center justify-center">
+          <Map />
+          </div>
         </div>
       </div>
     </div>
