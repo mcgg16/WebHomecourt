4203248d5b1170be4899007145117118fd82f1a9
Monica Guzman <a00838824@tec.mx>
2026-04-09 17:42:21 -0600

    Cambia barrita de color de calendar y más bonito component

--- WebHomecourt/src/components/Agenda/GameListItem.tsx
+++ WebHomecourt/src/components/Agenda/GameListItem.tsx
@@ -11,7 +11,7 @@ type GameProp = {
 }
 
 function GameListItem({ games }: GameProp) {
-  const homeBaseCSS = "flex flex-row justify-left bg-white rounded-lg outline-2 outline-gray-200 gap-5 mb-7 px-4 py-5 border-l-9"; // To inject css for home color bar
+  const homeBaseCSS = "flex flex-row justify-left bg-white rounded-lg outline-2 outline-gray-200 gap-5 mb-7 px-4 py-5 border-l-9 justify-center"; // To inject css for home color bar
 
   if (!games.length) {
     return <p>No games available for this month.</p>
@@ -21,28 +21,35 @@ function GameListItem({ games }: GameProp) {
     <div className="grid">
       <div>
         {games.map(game => (
-          <div key={game.game_id} className={`${homeBaseCSS} border-morado-lakers`}>
-            <img
-              src={game.logo_url}
-              alt={`Logo ${game.team_name}`}
-              className="w-[3.75rem] max-h-[3.75rem] mx-auto col-span-1"
-            />
-
-            {/* Name and date */}
-            <div>
-              <h3 className="font-black">vs {game.team_name} </h3>
-              <p>{format(parseISO(game.start_date), "EEE, dd MMM")}</p>
+          <div key={game.game_id} className={`${homeBaseCSS} ${game.home? "border-amarillo-oscuro" : "border-morado-lakers" }`}>
+            {/* Left side logo and team name */}
+            <div className="flex flex-row flex-1 items-center">
+              <img
+                src={game.logo_url}
+                alt={`Logo ${game.team_name}`}
+                className="w-[3.75rem] max-h-[3.75rem] object-contain flex-shrink-0"
+              />
+              {/* Name and date */}
+              <div className="flex flex-col items-start ml-3">
+                <h3 className="font-black">vs {game.team_name} </h3>
+                <p>{format(parseISO(game.start_date), "EEE, dd MMM")}</p>
+              </div>
             </div>
 
-            {/* Show whether won or lost */}
-            <SummaryScoreCard lakers_score={game.lakers_score} opposite_score={game.opposite_score} />
+            {/* Right side */}
+            <div className="flex flex-row items-center gap-2 flex-shrink-0">
+              {/* Show whether won or lost */}
+              <SummaryScoreCard lakers_score={game.lakers_score} opposite_score={game.opposite_score}/>
 
-            <Button
-              text="Recap"
-              type="primary"
-              onClick={() => {}} // Update to use redirect 
-              //className="w-full"
-            />
+              <Button
+                text="Recap"
+                type="primary"
+                onClick={() => {}} // Update to use redirect 
+                className="ml-5"
+              />
+            </div>
+
+            
 
           </div>
         ))}
